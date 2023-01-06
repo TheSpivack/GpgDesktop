e30e5d578c6339e0dafe50a32f1b9a68ece13238
Harris Spivack <harris.spivack@gmail.com>
2023-01-06 16:48:39 -0600

    create new dart package for gpg wrapper

---
+++ gpg_wrapper/example/gpg_wrapper_example.dart
@@ -0,0 +1,6 @@
+import 'package:gpg_wrapper/gpg_wrapper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ gpg_wrapper/test/gpg_wrapper_test.dart
@@ -0,0 +1,16 @@
+import 'package:gpg_wrapper/gpg_wrapper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
